c1bf52f4b2f291c06d3043f423fbb6df58a8808b
Felipe Augusto Silva <felipe.asilva.ext@panonline.com.br>
2023-11-27 17:50:25 -0300

    Initial Commit

---
+++ video-player-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video-player-app
+//
+//  Created by Felipe Augusto Silva on 27/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
